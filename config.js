cad224192e20ceac9f0ce25956be3d7448309f4f
Mihir Chaturvedi <plibither8@gmail.com>
2019-07-10 22:24:05 +0530

    Init: Initialise files

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	"base_url": "https://wakatime.com/api/v1/users/",
+	"username": process.env['WAKATIME_USERNAME'],
+	"api_key": process.env['WAKATIME_API_KEY'],
+	"start_date": ""
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+const CONFIG = require('./config');
+const fetch = require('node-fetch');
+
